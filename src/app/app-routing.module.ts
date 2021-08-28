e13776084bb936e6d4a8a3349c504cec2e3eda47
Nikolay Mihaylov <n.mihaylov@abv.bg>
2021-08-28 03:46:21 +0300

    pre final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CardComponent } from './card/card.component';
+import { AuthActivate } from './core/guards/auth.activate';
 
 const routes: Routes = [
+  // {
+  //   path: '404',
+  //   component: CardComponent,
+  //   canActivate: [AuthActivate],
+  //   data: {
+  //     authenticationRequired: false,
+  //     authenticationFailureRedirectUrl: '/'
+  //   }
+  // },
   {
-    path: '404',
-    component: CardComponent
-  },
-  {
-    path: '', 
+    path: 'home',
     component: CardComponent
   }
+  
 ];
 
 @NgModule({
